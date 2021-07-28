aec09a6b0777645535e75b195a2f6b5556e4e979
JatrovyKnedlicekLoL <adam@DESKTOP-ML9J4HQ>
2021-07-28 10:19:58 +0200

    oprava

--- src/main/java/net/mcreator/foodmod/procedures/RohlikwithblueberryjamFoodEatenProcedure.java
+++ src/main/java/net/mcreator/foodmod/procedures/RohlikwithblueberryjamFoodEatenProcedure.java
@@ -1,11 +1,19 @@
 package net.mcreator.foodmod.procedures;
 
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.foodmod.FoodModModElements;
+import net.mcreator.foodmod.FoodModMod;
+
+import java.util.Map;
+
 @FoodModModElements.ModElement.Tag
 public class RohlikwithblueberryjamFoodEatenProcedure extends FoodModModElements.ModElement {
-
 	public RohlikwithblueberryjamFoodEatenProcedure(FoodModModElements instance) {
 		super(instance, 61);
-
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -14,12 +22,8 @@ public class RohlikwithblueberryjamFoodEatenProcedure extends FoodModModElements
 				FoodModMod.LOGGER.warn("Failed to load dependency entity for procedure RohlikwithblueberryjamFoodEaten!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		if (entity instanceof LivingEntity)
 			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SPEED, (int) 2000, (int) 1));
-
 	}
-
 }
