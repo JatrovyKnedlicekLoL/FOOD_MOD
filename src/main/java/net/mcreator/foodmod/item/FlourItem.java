41119919162b3d8f08ffaf4ea0e59b1d7055570e
JatrovyKnedlicekLoL <adam@DESKTOP-ML9J4HQ>
2021-07-28 10:16:25 +0200

    všechny keře hotovi

--- src/main/java/net/mcreator/foodmod/item/FlourItem.java
+++ src/main/java/net/mcreator/foodmod/item/FlourItem.java
@@ -25,7 +25,7 @@ public class FlourItem extends FoodModModElements.ModElement {
 	}
 	public static class ItemCustom extends Item {
 		public ItemCustom() {
-			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.COMMON));
 			setRegistryName("flour");
 		}
 
